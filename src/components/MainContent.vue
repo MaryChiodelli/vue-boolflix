98d322d0e1f77c5009ef85868580876bfebf7507
MaryChiodelli <marychiodelli97@gmail.com>
2022-09-09 18:08:29 +0200

    add flag

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -5,7 +5,11 @@
       <li v-for="movie in movies" :key="movie.id">
         <p>{{ movie.title }}</p>
         <p>{{ movie.original_title }}</p>
-        <p>{{ movie.original_language }}</p>
+        <p>
+          <img v-if="movie.original_language === 'it'" src="../assets/it-flag.png" alt="">
+          <img v-else-if="movie.original_language === 'en'" src="../assets/en-flag.png" alt="">
+          <span v-else>{{ movie.original_language }}</span>
+        </p>
         <p>{{ movie.vote_average }}</p>
       </li>
     </ul>
@@ -24,7 +28,6 @@ export default {
     return {
       BASE_URI: 'https://api.themoviedb.org/3',
       api_key: '735e75087705e688a8302c51505637e4',
-      // query: '',
       movies: []
     }
   },
